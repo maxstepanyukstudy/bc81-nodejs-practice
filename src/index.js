089bf6c0fd0e413c2b43b97d7e10d57127636501
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-18 19:14:47 +0300

    upd index.js - node:path demo

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+import path from "node:path";
+
 const message = "Hello world";
 console.log(message);
+
+const pathToWorkDir = path.join(process.cwd());
+console.log(pathToWorkDir);
+
+const readmePath = path.join(pathToWorkDir, "README.md");
+console.log(readmePath);
+
+const readmePathParse = path.parse(readmePath);
+console.log(readmePathParse);
