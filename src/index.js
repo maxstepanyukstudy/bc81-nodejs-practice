f10a615c2797212e03564e1da696874dd510af57
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-18 21:10:58 +0300

    upd index.js - add express boilerplate

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
+import express from "express";
+
 const message = "Hello world";
 console.log(message);
+const PORT = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("hostname:", req.hostname);
+  res.status(200).json({
+    message: "Hello world",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
