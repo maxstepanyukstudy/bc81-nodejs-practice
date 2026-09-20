ae7db6dfb0b2bafbe5a018e37a84f4982085b44b
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-20 15:23:21 +0300

    upd index.js - add app.use(cors());

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const message = "Hello world";
 console.log(message);
@@ -7,6 +8,7 @@ const PORT = 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const usersList = [
   { id: 1, name: "Alice" },
