54cba6bbe5d427db68fec493c03ce0228ed823e5
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-20 12:46:50 +0300

    upd index.js - add GET /users , GET /users/:userId , userList

--- src/index.js
+++ src/index.js
@@ -6,11 +6,28 @@ const PORT = 3000;
 
 const app = express();
 
+const usersList = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+];
+
 app.get("/", (req, res) => {
   console.log("hostname:", req.hostname);
   res.status(200).json({
     message: "Hello world",
   });
+});
+
+app.get("/users", (req, res) => {
+  console.log("/users", "hostname:", req.hostname);
+  res.status(200).json(usersList);
+});
+
+app.get("/users/:userId", (req, res) => {
+  const id = Number(req.params.userId);
+  console.log("/users/" + id, "hostname:", req.hostname);
+  const user = usersList.find((user) => user.id === id);
+  res.status(200).json(user);
 });
 
 app.listen(PORT, () => {
